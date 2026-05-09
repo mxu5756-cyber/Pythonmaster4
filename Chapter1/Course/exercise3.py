5857a304eec2ad77f19e112459a4fea55effd66b
lucie <lucielixiaoya1317@gmail.com>
2026-05-09 13:31:10 +0200

    week 4

---
+++ Chapter1/Course/exercise3.py
@@ -0,0 +1,9 @@
+class Phone:
+    __is_5g_enable=None
+    def __chek_5g(self):
+        if self. __is_5g_enable==True:
+            print("5g activité")
+        else:
+            print("5g off , utilisation de réseau ")
+def call_by_5g():
+    print()
